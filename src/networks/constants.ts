9899e03ddcf06a0f4b09edb9bca6446d211418fc
steven2308 <steven2308@outlook.com>
2023-07-24 10:03:27 -0500

    Adds polygon and updates starting blocks.
    Also adds .env.example file.

--- src/networks/constants.ts
+++ src/networks/constants.ts
@@ -2,28 +2,34 @@ export const ETH_MAINNET_ARCHIVE='https://v2.archive.subsquid.io/network/ethereu
 export const ETH_MAINNET_RPC=process.env.ETH_MAINNET_RPC || ''
 export const ETH_MAINNET_CHAIN_ID=1
 export const ETH_MAINNET_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const ETH_MAINNET_INIT_BLOCK=17577219
+export const ETH_MAINNET_INIT_BLOCK=17763459
 
 export const ETH_SEPOLIA_ARCHIVE='https://v2.archive.subsquid.io/network/ethereum-sepolia'
 export const ETH_SEPOLIA_RPC=process.env.ETH_SEPOLIA_RPC || ''
 export const ETH_SEPOLIA_CHAIN_ID=11155111
 export const ETH_SEPOLIA_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const ETH_SEPOLIA_INIT_BLOCK=3917929
+export const ETH_SEPOLIA_INIT_BLOCK=3932466
 
 export const ARBITRUM_GOERLI_ARCHIVE='https://v2.archive.subsquid.io/network/arbitrum-goerli'
 export const ARBITRUM_GOERLI_RPC=process.env.ARBITRUM_GOERLI_RPC || ''
 export const ARBITRUM_GOERLI_CHAIN_ID=5
 export const ARBITRUM_GOERLI_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const ARBITRUM_GOERLI_INIT_BLOCK=29666115
+export const ARBITRUM_GOERLI_INIT_BLOCK=31185484
 
 export const MOONRIVER_ARCHIVE='https://v2.archive.subsquid.io/network/moonriver-mainnet'
 export const MOONRIVER_RPC=process.env.MOONRIVER_RPC || ''
 export const MOONRIVER_CHAIN_ID=1285
 export const MOONRIVER_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const MOONRIVER_INIT_BLOCK=4620073
+export const MOONRIVER_INIT_BLOCK=4741651
 
 export const MOONBEAM_ARCHIVE='https://v2.archive.subsquid.io/network/moonbeam-mainnet'
 export const MOONBEAM_RPC=process.env.MOONBEAM_RPC || ''
 export const MOONBEAM_CHAIN_ID=1284
 export const MOONBEAM_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const MOONBEAM_INIT_BLOCK=3877009
+export const MOONBEAM_INIT_BLOCK=4062888
+
+export const POLYGON_ARCHIVE='https://v2.archive.subsquid.io/network/polygon-mainnet'
+export const POLYGON_RPC=process.env.POLYGON_RPC || ''
+export const POLYGON_CHAIN_ID=137
+export const POLYGON_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
+export const POLYGON_INIT_BLOCK=45465948
