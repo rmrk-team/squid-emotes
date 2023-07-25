ddc739dfeed15c23062958e88e1c733721be24ba
steven2308 <steven2308@outlook.com>
2023-07-25 09:25:50 -0500

    Upadtes init blocks for different networks.

--- src/networks/constants.ts
+++ src/networks/constants.ts
@@ -2,7 +2,7 @@ export const ETH_MAINNET_ARCHIVE='https://v2.archive.subsquid.io/network/ethereu
 export const ETH_MAINNET_RPC=process.env.ETH_MAINNET_RPC || ''
 export const ETH_MAINNET_CHAIN_ID=1
 export const ETH_MAINNET_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const ETH_MAINNET_INIT_BLOCK=17763459
+export const ETH_MAINNET_INIT_BLOCK=17770688
 
 export const ETH_SEPOLIA_ARCHIVE='https://v2.archive.subsquid.io/network/ethereum-sepolia'
 export const ETH_SEPOLIA_RPC=process.env.ETH_SEPOLIA_RPC || ''
@@ -20,16 +20,16 @@ export const MOONRIVER_ARCHIVE='https://v2.archive.subsquid.io/network/moonriver
 export const MOONRIVER_RPC=process.env.MOONRIVER_RPC || ''
 export const MOONRIVER_CHAIN_ID=1285
 export const MOONRIVER_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const MOONRIVER_INIT_BLOCK=4741651
+export const MOONRIVER_INIT_BLOCK=4715430
 
 export const MOONBEAM_ARCHIVE='https://v2.archive.subsquid.io/network/moonbeam-mainnet'
 export const MOONBEAM_RPC=process.env.MOONBEAM_RPC || ''
 export const MOONBEAM_CHAIN_ID=1284
 export const MOONBEAM_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const MOONBEAM_INIT_BLOCK=4062888
+export const MOONBEAM_INIT_BLOCK=4065179
 
 export const POLYGON_ARCHIVE='https://v2.archive.subsquid.io/network/polygon-mainnet'
 export const POLYGON_RPC=process.env.POLYGON_RPC || ''
 export const POLYGON_CHAIN_ID=137
 export const POLYGON_EMOTES_REPO_ADDRESS='0x31107356a47DD65e832676C1FCBECb0B7C075f5B'
-export const POLYGON_INIT_BLOCK=45465948
+export const POLYGON_INIT_BLOCK=45477725
